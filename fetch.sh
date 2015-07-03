45a8359f55cdc12a44395abbf5180ca707d02e01
Brenda Wallace <brenda@brenda.nz>
2015-07-04 11:45:50 +1200

    fetch with api key

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-$APIKEY=$(cat "apikey.txt") 
+APIKEY=$(cat "apikey.txt") 
+
 curl -g http://api.digitalnz.org/records.json\?api_key\=$APIKEY\&and\[primary_collection\]\=New+Zealand+Gazette\&per_page\=100\&text\="appointments/reappointments"\&fields\=title,description,date,id\&sort\=date\&direction\=desc | python -m json.tool > gazette.json && cat gazette.json | grep result_count
